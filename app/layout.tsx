b178f8e8f6829b332cf5a4a0cd0135d91cb2f310
Azeem <bljazeem@gmail.com>
2025-09-13 20:54:39 +0100

    refactor: Remove top padding from body in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${opensans.variable} antialiased pt-16`}>
+      <body className={`${opensans.variable} antialiased `}>
         <ClientProviders>
           <AuthInitializer>
             <Header />
